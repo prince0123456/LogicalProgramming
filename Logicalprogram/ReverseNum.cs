716a982c47be339eeb665685e4647393b31071c2
Prince Gupta <princegupta.pg46@gmail.com>
2023-05-11 14:41:46 +0530

    Reverse Num

---
+++ Logicalprogram/ReverseNum.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logicalprogram
+{
+    public class ReverseNum
+    {
+        public static void reverseNumber()
+        {
+            int n, rev = 0, rem;
+            Console.WriteLine("enter num");
+            n = Convert.ToInt32(Console.ReadLine());
+            while (n != 0)
+            {
+                rem = n % 10;
+                rev = rev * 10 + rem;
+                n = n / 10;
+            }
+            Console.WriteLine("Reversed Number" + rev);
+        }
+    }
+}
