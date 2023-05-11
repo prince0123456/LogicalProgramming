c560ed1fafce78f1d065189c1e49cfc25a75c674
Prince Gupta <princegupta.pg46@gmail.com>
2023-05-11 14:26:47 +0530

    Coupon

--- Logicalprogram/Coupons.cs
+++ Logicalprogram/Coupons.cs
@@ -8,29 +8,26 @@ namespace Logicalprogram
 {
     internal class Coupons
         {
-            /// <summary>
-            /// Initializes a new instance of the <see cref="Coupons"/> class.
-            /// </summary>
-            public Coupons()
-            {
-                int distinct = 0, count = 0;
-                Console.WriteLine("Enter a number of given distinct coupons:");
-                int couponNo = Convert.ToInt32(Console.ReadLine());
-                bool[] isCollected = new bool[couponNo];
-                while (distinct < couponNo)
-                {
-                    Random random = new Random();
-                    int newCoupon = (int)(random.NextDouble() * couponNo);
-                    count++;
-                    if (!isCollected[newCoupon])
-                    {
-                        distinct++;
-                        isCollected[newCoupon] = true;
-                    }
-                }
 
-                Console.WriteLine("Total random number needed to have all distinct coupons:" + count);
+        public static void coupon()
+        {
+            int distinct = 0, count = 0;
+            Console.WriteLine("Enter a number of given distinct coupons:");
+            int couponNo = Convert.ToInt32(Console.ReadLine());
+            bool[] isCollected = new bool[couponNo];
+            while (distinct < couponNo)
+            {
+                Random random = new Random();
+                int newCoupon = (int)(random.NextDouble() * couponNo);
+                count++;
+                if (!isCollected[newCoupon])
+                {
+                    distinct++;
+                    isCollected[newCoupon] = true;
+                }
             }
+
+            Console.WriteLine("Total random number needed to have all distinct coupons:" + count);
         }
     }
 }
